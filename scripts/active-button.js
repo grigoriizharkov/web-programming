1b9b6c35faf5f6f45f4bd42bac0f86fff38a1a36
Grigory Zharkov <grigorii_al@mail.ru>
2022-12-02 15:08:54 +0300

    fix: active button script type mismatch

--- scripts/active-button.js
+++ scripts/active-button.js
@@ -2,16 +2,13 @@ window.addEventListener("load", function () {
     let buttons = document.querySelectorAll("div.navigation a")
 
     function active() {
-        let path = window.location.href;
-        let current_element = path.split('/');
-
-        if (current_element.length > 1) {
-            current_element = current_element[4]
-        }
+        let path = window.location.href
+        let current_element_path = path.split('/')
+        let current_element = current_element_path[4]
 
         for (let i = 0; i < buttons.length; ++i) {
             let button_href = buttons[i].href.split('/')[4]
-            if (current_element[i] === button_href) {
+            if (current_element === button_href) {
                 buttons[i].classList.add("active")
             } else {
                 buttons[i].classList.remove("active")
