a5e9a2a8369de500844a8c40ccd54d9928ddfad1
Grigory Zharkov <grigorii_al@mail.ru>
2022-12-03 16:35:26 +0300

    refactor: naming and code style

--- scripts/active-button.js
+++ scripts/active-button.js
@@ -3,16 +3,12 @@ window.addEventListener("load", function () {
 
     function active() {
         let path = window.location.href
-        let current_element_path = path.split('/')
-        let current_element = current_element_path[4]
+        let current_page_path = path.split('/')
+        let current_page = current_page_path[4]
 
         for (let i = 0; i < buttons.length; ++i) {
             let button_href = buttons[i].href.split('/')[4]
-            if (current_element === button_href) {
-                buttons[i].classList.add("active")
-            } else {
-                buttons[i].classList.remove("active")
-            }
+            if (current_page === button_href) { buttons[i].classList.add("active") }
         }
     }
 
